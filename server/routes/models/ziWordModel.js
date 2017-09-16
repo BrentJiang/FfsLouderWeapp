78441f4acfd61e9b07472244c1e4661c77ab620f
brent jiang <sanyi0127@sina.com>
2017-09-16 15:31:05 +0800

    撒旦发

--- server/routes/models/ziWordModel.js
+++ server/routes/models/ziWordModel.js
@@ -1,3 +1,5 @@
 'use strict';
-
-module.exports = require('./ziWordModelTxt');
+var resolve = require('path').resolve
+var res = resolve('./routes/models/ziWordModelTxt');
+console.log("txt path: " + res);
+module.exports = require(res);
