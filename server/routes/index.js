a61f1ed33477018a5de7fb22baea7ae0155d58ae
brent jiang <sanyi0127@sina.com>
2017-09-16 15:35:04 +0800

    阿斯大法

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,13 +7,46 @@ var resolve = require('path').resolve
 const express = require('express');
 const router = express.Router();
 
-console.log("controller path: " + resolve('./routes/controllers/ziWordController'))
-var ziWordController = require(resolve('./routes/controllers/ziWordController'));
+var datapath = resolve('./routes/data/chineseLetters.txt');
+console.log("data path: " + datapath);
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(datapath)
+});
+var chineseDict = {};
+var chineseLetterCounter = 0;
+lineReader.on('line', function (line) {
+  //console.log('Line from file:', line);
+  var letter = line.charAt(0);
+  var itemval = {
+    "id": ++chineseLetterCounter,
+    "letter": letter,
+    "Interpretation": line.substr(2)
+  };
+  if (chineseLetterCounter < 10) {
+    console.log(line);
+  } else {
+    //return; // for test
+  }
+  chineseDict[letter] = itemval;
+});
+
+console.log('after calling readFile chineseLetters.txt');
+
+function getZiWord(letter, callback) {
+  console.log('query from txt: ' + letter);
+  if (letter in chineseDict) {
+    console.log("find it!");
+    callback(null, [chineseDict[letter]]);
+  } else {
+    console.log("not found!");
+    callback({ "result": "not found." }, null);
+  }
+}
 
 router.get('/', require('./welcome'));
 router.get('/login', require('./login'));
 router.get('/user', require('./user'));
-router.get('/letter/:letter', ziWordController.get_zi_word);
+router.get('/letter/:letter', getZiWord);
 router.all('/tunnel', require('./tunnel'));
 
 module.exports = router;
