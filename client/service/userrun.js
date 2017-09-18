6b908cb458b37341eba0a5f886730a0d43632a7e
brent jiang <sanyi0127@sina.com>
2017-09-18 23:48:02 +0800

    的

---
+++ client/service/userrun.js
@@ -0,0 +1,27 @@
+'use restrict'
+
+// 引入 QCloud 小程序增强 SDK
+var qcloud = require('../vendor/qcloud-weapp-client-sdk/index');
+
+// 引入配置
+var config = require('../config');
+
+var timeout = null;
+
+function checkUserTimeout() {
+    qcloud.request({
+        url: `https://${config.service.host}/user`,
+        login: true,
+        success: (response) => {
+        }
+    });
+}
+
+function initializeClient(openid) {
+  console.log(`openid was => ${openid}`);
+  timeout = setInterval(checkUserTimeout, 1500);
+}
+
+module.exports = {
+    initializeClient: initializeClient
+};
